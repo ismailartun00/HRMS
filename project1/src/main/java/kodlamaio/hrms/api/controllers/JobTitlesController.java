2c9c911ab636a870c62bc6943964a7b3a7bb718f
ismail <ismailartun00@gmail.com>
2021-05-10 17:29:31 +0300

    first commit of the project

---
+++ project1/src/main/java/kodlamaio/hrms/api/controllers/JobTitlesController.java
@@ -0,0 +1,27 @@
+package kodlamaio.hrms.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.JobTitleService;
+import kodlamaio.hrms.entities.concretes.JobTitle;
+
+@RestController
+@RequestMapping("/api/jobtitle")
+public class JobTitlesController {
+	
+	@Autowired
+	private JobTitleService jobTitleService;
+	
+	
+	@GetMapping("/getall")
+	public List<JobTitle> getAll(){
+		return this.jobTitleService.getAll();
+	}
+
+}
